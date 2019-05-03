e61ee96a8209fe1fa613fa79ccac2023bd083155
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2019-05-03 14:36:23 +0100

    WASD now works.

--- MyTextAdventureGame/Source.cpp
+++ MyTextAdventureGame/Source.cpp
@@ -1,25 +1,81 @@
+#include <cstdlib>
 #include <iostream>
 
-#include <string>
-
-int main();
-
-using std::cout;
-using std::endl;
-using std::cin;
-
-// DECLARE functions
-void wait_for_keypress(void);
+void playerMove(char[3][4], int, int);
+void clrscr();
 
 int main()
 {
-	cout << "Hello World" << endl;
-	wait_for_keypress();
+	int x, y;
+	char map[3][4] =
+	{
+		{'A','B','C','D'},
+		{'E','F','G','H'},
+		{'I','J','K','L'}
+	};
+
+	x = 0;
+	y = 0;
+
+	std::cout << "Use 'w', 'a', 's', 'd' to move around the array of letters";
+	std::cout << std::endl << std::endl;
+
+	playerMove(map, x, y);
+
+	return 0;
 }
 
-// DEFINE functions
-void wait_for_keypress(void)
+void playerMove(char map[3][4], int x, int y)
 {
-	cout << "Press any key to continue" << endl;
-	cin.get();
+	char input;
+	int error = 0;
+
+	std::cout << "You are currently at location " << map[y][x];
+	std::cout << std::endl << "Enter a direction of movement." << std::endl;
+	std::cin >> input;
+
+	switch (input)
+	{
+	case 'w':
+		y = y - 1;
+		break;
+	case 's':
+		y = y + 1;
+		break;
+	case 'd':
+		x = x + 1;
+		break;
+	case 'a':
+		x = x - 1;
+		break;
+	case '\n':
+		break;
+	default:
+		error = 1;
+		break;
+
+	}
+
+	if (x < 0 || y < 0 || x >(4 - 1) || y >(3 - 1))
+	{
+		x = (x > (4 - 1)) ? (4 - 1) : x;
+		x = (x < 0) ? 0 : x;
+		y = (y > (3 - 1)) ? (3 - 1) : y;
+		y = (y < 0) ? 0 : y;
+		error = 1;
+	}
+
+	clrscr();
+
+	if (error == 1)
+		std::cout << "YOU CAN'T GO THAT WAY!" << std::endl;
+
+	playerMove(map, x, y);
+}
+
+void clrscr()
+{
+	system("cls"); //clear for mac
+
+	return;
 }
